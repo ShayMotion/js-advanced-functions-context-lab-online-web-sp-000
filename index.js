f3a608fa8ebd90fc83e4e6d37b99f3c3e5749e0f
Shayan Mohsin <shaymotionfilms@gmail.com>
2021-03-05 14:45:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,3 +20,77 @@ let allWagesFor = function () {
 
     return payable
 }
+
+function createEmployeeRecord(firstName, familyName, title, payPerHour) {
+  array = [];
+  array[0] = firstName;
+  array[1] = familyName;
+  array[2] = payPerHour;
+  timeInEvents = [];
+  timeOutEvents = [];
+}
+
+function createEmployeeRecord(row) {
+return {
+  firstName: row[0],
+  familyName: row[1],
+  title: row[2],
+  payPerHour: row[3],
+  timeInEvents: [],
+  timeOutEvents: []
+  };
+}
+
+let createEmployeeRecords = function(employeeRowData) {
+    return employeeRowData.map(function(row){
+        return createEmployeeRecord(row);
+    });
+};
+
+let createTimeInEvent = function(employee, dateStamp){
+  let [date, hour] = dateStamp.split('');
+  
+  employee.timeInEvents.push({
+    type: "TimeIn",
+    hour: parseInt(hour, 10),
+    date,
+  });
+};
+
+let timeOutEvent = function(employee, dateStamp){
+  let [date, hour = dateStamp.split('')
+  employee.timeOutEvents.push({
+    type: "TimeOut",
+    hour: parseInt(hour, 10),
+    date,
+  })
+  return employee
+})
+
+let wagesEarnedOnDate = function(employee, specificDate)
+let rawWage = hoursWorkedOnDate(employee, specificDate)
+* employee.payPerHour
+return parseFloat(rawWage.toString())
+}
+
+let allWagesFor = function(employee){
+  let daysWorked - employee.timeInEvents.map(function(e){return e.date 
+  })
+let payable = daysWorked.reduce(function(memo, d){
+  return memo + wagesEarnedOnDate(employee, d)
+}, 0)
+return payable
+}
+
+let findEmployeeByFirstName = function(srcArray, firstName) {
+  return srcArray.find(function(rec){
+    return rec.firstName === firstName
+  })
+}
+
+let calculatePayroll = function(arrayOfEmployeeRecords){
+  return arrayOfEmployeeRecords.reduce(function(memo, re) {
+    return memo + allWagesFor(rec)
+  
+}, 0)
+}
