d9e15a5da150af28dc52152da52eb843a14e8026
Shayan Mohsin <shaymotionfilms@gmail.com>
2021-03-05 14:47:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,60 @@
-/* Your Code Here */
+const createEmployeeRecord = record => {
+  return {
+    firstName: record[0],
+    familyName: record[1],
+    title: record[2],
+    payPerHour: record[3],
+    timeInEvents: [],
+    timeOutEvents: []
+  }
+}
+
+const createEmployeeRecords = records => {
+  return records.map(record => createEmployeeRecord(record));
+}
+
+const createTimeInEvent = function (dateStamp) {
+  const [date, hour] = dateStamp.split(' ');
+  this.timeInEvents.push({
+    type: 'TimeIn',
+    hour: parseInt(hour, 10),
+    date: date
+  })
+  return this;
+}
+
+const createTimeOutEvent = function (dateStamp) {
+  const [date, hour] = dateStamp.split(' ');
+  this.timeOutEvents.push({
+    type: 'TimeOut',
+    hour: parseInt(hour, 10),
+    date: date
+  })
+  return this;
+}
+
+const hoursWorkedOnDate = function (date) {
+  const inEvent = this.timeInEvents.find(e => e.date === date);
+  const outEvent = this.timeOutEvents.find(e => e.date === date);
+  return (outEvent.hour - inEvent.hour) / 100;
+}
+
+const wagesEarnedOnDate = function (date) {
+  return hoursWorkedOnDate.call(this, date) * this.payPerHour;
+}
+
+const findEmployeeByFirstName = function (srcArray, firstName) {
+  return srcArray.find(employee => employee.firstName === firstName);
+}
+
+const calculatePayroll = function (employees) {
+  return employees.reduce((total, employee) => total + allWagesFor.call(employee), 0);
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
  sneaky bug that we'll cover in the next few lessons!
-
  As a result, the lessons for this function will pass *and* it will be available
  for you to use if you need it!
  */
@@ -19,78 +69,4 @@ let allWagesFor = function () {
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
-}
-
-function createEmployeeRecord(firstName, familyName, title, payPerHour) {
-  array = [];
-  array[0] = firstName;
-  array[1] = familyName;
-  array[2] = payPerHour;
-  timeInEvents = [];
-  timeOutEvents = [];
-}
-
-function createEmployeeRecord(row) {
-return {
-  firstName: row[0],
-  familyName: row[1],
-  title: row[2],
-  payPerHour: row[3],
-  timeInEvents: [],
-  timeOutEvents: []
-  };
-}
-
-let createEmployeeRecords = function(employeeRowData) {
-    return employeeRowData.map(function(row){
-        return createEmployeeRecord(row);
-    });
-};
-
-let createTimeInEvent = function(employee, dateStamp){
-  let [date, hour] = dateStamp.split('');
-  
-  employee.timeInEvents.push({
-    type: "TimeIn",
-    hour: parseInt(hour, 10),
-    date,
-  });
-};
-
-let timeOutEvent = function(employee, dateStamp){
-  let [date, hour = dateStamp.split('')
-  employee.timeOutEvents.push({
-    type: "TimeOut",
-    hour: parseInt(hour, 10),
-    date,
-  })
-  return employee
-})
-
-let wagesEarnedOnDate = function(employee, specificDate)
-let rawWage = hoursWorkedOnDate(employee, specificDate)
-* employee.payPerHour
-return parseFloat(rawWage.toString())
-}
-
-let allWagesFor = function(employee){
-  let daysWorked - employee.timeInEvents.map(function(e){return e.date 
-  })
-let payable = daysWorked.reduce(function(memo, d){
-  return memo + wagesEarnedOnDate(employee, d)
-}, 0)
-return payable
-}
-
-let findEmployeeByFirstName = function(srcArray, firstName) {
-  return srcArray.find(function(rec){
-    return rec.firstName === firstName
-  })
-}
-
-let calculatePayroll = function(arrayOfEmployeeRecords){
-  return arrayOfEmployeeRecords.reduce(function(memo, re) {
-    return memo + allWagesFor(rec)
-  
-}, 0)
 }
